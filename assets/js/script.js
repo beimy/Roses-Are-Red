836df6a5b5eb7180fc37a1004515e8cedf4538c1
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-06 16:47:01 -0400

    appended brewery api data onto webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,32 +57,33 @@ function eventSearch_handleClick() {
     
 };
 
-function fetchEvent(searchOptions) {
-    // fetch event data from ticket master api
-    let ticket_api = `https://app.ticketmaster.com/discovery/v2/events.json?size=20&city=${searchOptions.city}&localStartDateTime=${searchOptions.startDate}T00:00:01,${searchOptions.endDate}T23:59:59&classificationName=${searchOptions.category}&apikey=${ticket_api_key}`;
-    fetch(ticket_api)
-        .then(data => data.json())
-        .then (data => {
-            // jd = data;
-            console.log(data);
-            let events = data._embedded.events;
-            events.forEach(event => {
-                // prints out in this format: April 11th, 2022 @ 7:30pm
-                eventDateTime = moment(`${event.dates.start.dateTime}`).format('MMMM Do, YYYY @ hh:mm a');
-                document.querySelector(".results").innerHTML += 
-                `<a href="${event.url}" class="card">
-                        <img src="${event.images[0].url}">
-                        <div class="is-size-6">
-                        <h4><span class="has-text-dark-red has-background-white">Event Name:</span> ${event.name}</h4>
-                        <h4><span class="has-text-dark-red has-background-white">Event Classification:</span> ${event.classifications[0].segment.name}</h4>
-                        <h4><span class="has-text-dark-red has-background-white">Event Date & Time:</span> ${eventDateTime}</h4>
-                        <h4><span class="has-text-dark-red has-background-white">Event Venue:</span> ${event._embedded.venues[0].name}</h4>
-                        <h4><span class="has-text-dark-red has-background-white">Event Address:</span> ${event._embedded.venues[0].address.line1}</h4>
-                        <h4>${event._embedded.venues[0].city.name}, ${event._embedded.venues[0].state.stateCode}. ${event._embedded.venues[0].postalCode}</h4>
-                        </div>
-                    </a>`
-            });
-        }) 
+        function fetchEvent(searchOptions) {
+            // fetch event data from ticket master api
+            let ticket_api = `https://app.ticketmaster.com/discovery/v2/events.json?size=20&city=${searchOptions.city}&localStartDateTime=${searchOptions.startDate}T00:00:01,${searchOptions.endDate}T23:59:59&classificationName=${searchOptions.category}&apikey=${ticket_api_key}`;
+            fetch(ticket_api)
+                .then(data => data.json())
+                .then (data => {
+                    // jd = data;
+                    console.log(data);
+                    let events = data._embedded.events;
+                    events.forEach(event => {
+                        // prints out in this format: April 11th, 2022 @ 7:30pm
+                        eventDateTime = moment(`${event.dates.start.dateTime}`).format('MMMM Do, YYYY @ hh:mm a');
+                        document.querySelector(".event-results").innerHTML += 
+                        `<a href="${event.url}" class="card">
+                                <img src="${event.images[0].url}">
+                                <div class="is-size-6">
+                                <h4><span class="has-text-dark-red has-background-white">Event Name:</span> ${event.name}</h4>
+                                <h4><span class="has-text-dark-red has-background-white">Event Classification:</span> ${event.classifications[0].segment.name}</h4>
+                                <h4><span class="has-text-dark-red has-background-white">Event Date & Time:</span> ${eventDateTime}</h4>
+                                <h4><span class="has-text-dark-red has-background-white">Event Venue:</span> ${event._embedded.venues[0].name}</h4>
+                                <h4><span class="has-text-dark-red has-background-white">Event Address:</span> ${event._embedded.venues[0].address.line1}</h4>
+                                <h4>${event._embedded.venues[0].city.name}, ${event._embedded.venues[0].state.stateCode}. ${event._embedded.venues[0].postalCode}</h4>
+                                </div>
+                            </a>`
+
+                    });
+                }) 
     
     // fetch lat and lon from open weather map api
     let url = `https://api.openweathermap.org/geo/1.0/direct?q=${searchOptions.city}&limit=1&appid=${api_key}`
@@ -96,16 +97,23 @@ function fetchEvent(searchOptions) {
     fetch(brew_api)
         .then(data => data.json())
         .then (data => {
+            jd = data;
             console.log(data);
+            for (var i = 0; i <= data.length - 1; i++) {
+                console.log(data[i])
+                document.querySelector(".brew-results").innerHTML += 
+                `<a href="${data[i].website_url}" class="card">
+                        <div class="is-size-6">
+                        <h4><span class="has-text-dark-red has-background-white">Brewery Name:</span> ${data[i].name}</h4>
+                        <h4><span class="has-text-dark-red has-background-white">Type:</span> ${data[i].brewery_type}</h4>   
+                        <h4><span class="has-text-dark-red has-background-white">Address:</span> ${data[i].street}, ${data[i].city}, ${data[i].state}. ${data[i].postal_code}</h4>
+                        <h4><span class="has-text-dark-red has-background-white">Phone Number:</span> ${data[i].phone}</h4>    
+                        </div>
+                    </a>`
+            }
         });
     }); 
 }
-
-
-
-
-
-
 
 
 // function fetchYelp(yelpSearch_Params) {
